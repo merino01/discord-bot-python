dbaee3c3f25c4bb4f22af09fbf41dd4c15e7abf6
AleTorres2k <alextorresalonso@hotmail.es>
2025-05-17 13:44:57 +0200

    [FEATURE] Mejoras implementadas en la funcionalidad de solicitud de desbaneo

--- modules/unbans/slash_commands.py
+++ modules/unbans/slash_commands.py
@@ -1,57 +1,93 @@
 from discord import (
-    app_commands,
-    Interaction,
-    TextChannel,
-    Object,
-    Embed,
-    Color,
+	app_commands,
+	Interaction,
+	TextChannel,
+	Object,
+	Embed,
+	Color,
 	Member,
 	ui,
-	TextStyle
+	TextStyle,
+	ButtonStyle,
+	utils
 )
+
 from discord.ext import commands
 from settings import guild_id
+from datetime import datetime, timezone
+
+class MyButtonsView(ui.View):
+
+	def __init__(self, member: Member):
+		super().__init__()
+		self.member = member
+
+	@ui.button(label="Aceptar", style=ButtonStyle.green)
+	async def onAccept(self, interaction: Interaction, button: ui.Button):
+
+		try:
+			await self.disable_all_buttons(interaction)
+			await interaction.followup.send(content="Se ha aceptado la solicitud de desbaneo del usuario: "+ self.member.display_name +" por: "+interaction.user.display_name, ephemeral=True)
+		except Exception as exception:
+			print(exception)
+
+	@ui.button(label="Rechazar", style=ButtonStyle.red)
+	async def onReject(self, interaction: Interaction, button: ui.Button):
+
+		try:
+			await self.disable_all_buttons(interaction)
+			await interaction.followup.send(content="Se ha rechazado la solicitud de desbaneo del usuario: "+ self.member.display_name +" por: "+interaction.user.display_name, ephemeral=True)
+		except Exception as exception:
+			print(exception)
+
+	async def disable_all_buttons(self, interaction: Interaction):
+		for btn in self.children:
+			btn.disabled = True
+		await interaction.response.edit_message(content="Todos los botones han sido deshabilitados.", view=self)
 
 class UnbanModal(ui.Modal, title="Solicitud de desbaneo"):
 
-    razon = ui.TextInput(
-        label="¿Por qué deberías ser desbaneado?",
-        style=TextStyle.paragraph,
-        placeholder="Explica brevemente...",
-        required=True,
-        max_length=300,
-    )
+	razon = ui.TextInput(
+		label="¿Por qué deberías ser desbaneado?",
+		style=TextStyle.paragraph,
+		placeholder="Explica brevemente...",
+		required=True,
+		max_length=300,
+	)
 
-    def __init__(self, member: Member):
-        super().__init__()
-        self.member = member
+	def __init__(self, member: Member):
+		super().__init__()
+		self.member = member
 
-    async def on_submit(self, interaction: Interaction):
-        embed = Embed(
-            colour=Color.red(),
-            title=f"Solicitud desbaneo de: {self.member.display_name}",
-            description=self.razon.value
-        )
-        embed.set_footer(text="Bot Dead by daylight España")
-        embed.set_author(name="ID usuario: "+str(self.member.id))
-        embed.set_thumbnail(url=self.member.display_avatar.url)
+	async def on_submit(self, interaction: Interaction):
 
-        await interaction.response.send_message(embed=embed)
+		embed = Embed(
+			colour=Color.red(),
+			title=f"Solicitud desbaneo de: {self.member.display_name}",
+			description=self.razon.value,
+			timestamp= datetime.now(timezone.utc)
+		)
+		embed.set_footer(text="Bot Dead by daylight España")
+		embed.set_author(name="ID usuario: "+str(self.member.id))
+		embed.set_thumbnail(url=self.member.display_avatar.url)
+		view = MyButtonsView(self.member)
 
-class UnbanCommands(commands.GroupCog, name="desbaneo"):
-    
+		await interaction.response.send_message(embed=embed, view=view)
+
+class UnbanCommands(commands.GroupCog, name="solicitud"):
+	
 	def __init__(self, bot):
 		self.bot = bot
 
 	##########################################################
-    ### Comando para mandar una solicitud de desbaneo ########
-    ##########################################################
+	### Comando para mandar una solicitud de desbaneo ########
+	##########################################################
 
-	@app_commands.command(name="unban", description="Solicitar desbaneo a Dead by daylight España")
+	@app_commands.command(name="desbaneo", description="Solicitar desbaneo a Dead by daylight España")
 	@app_commands.checks.has_permissions(
-        manage_channels=True,
-        manage_messages=True
-    )
+		manage_channels=True,
+		manage_messages=True
+	)
 	async def generateModal(
 		self,
 		interaction: Interaction,
